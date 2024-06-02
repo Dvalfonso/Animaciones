cd216a436ddc1c5d7eac7b4b8048b3e530f60da3
David <davidalfonsook@gmail.com>
2024-06-01 22:56:10 -0300

    Agregue el archivo de configuracion

---
+++ conf.lua
@@ -0,0 +1,13 @@
+function love.conf(t)
+    t.identity = "data/saves"
+    t.version = "11.5"
+    t.console = false
+    t.window.title = "Sin titulo"
+    t.window.icon = "sprites/logo.png" -- No se ve
+    t.window.width = 800
+    t.window.height = 480
+    t.window.resizable = true
+    t.window.minWidth = 800
+    t.window.minheight = 480
+    t.window.borderless = false
+end
